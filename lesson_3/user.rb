1d529cb2887ec5001c920d97aa6ec78273b0abb9
Maks Fedorovskiy <mvfedorovskiy@gmail.com>
2021-09-03 16:30:24 +0000

    lesson_3

---
+++ lesson_3/user.rb
@@ -0,0 +1,25 @@
+##
+# Клас User для работы с пользователем
+class User
+  ##
+  # Вывод ФИО 
+  #
+  # = Пример использования
+  #
+  # u = User.new
+  # u.fio
+  def fio
+    'Василий Батарейкин'
+  end
+  
+  ##
+  # Вывод профессии 
+  #
+  # = Пример использования
+  #
+  # u = User.new
+  # u.profession
+  def profession 
+    'Rubby programmer'
+  end
+end
